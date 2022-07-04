844cf8f7df34ae2dedd6eb310c575aa981960c70
Wairioko <charlesmungai5@gmail.com>
2022-07-04 05:45:33 -0700

    Adding file 1-element_at.py

--- 1-element_at.py
+++ 1-element_at.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
-        return (None)
-    length = len(my_list)
-    if idx > length - 1:
-        return (None)
-    return (my_list[idx])
+    for idx in my_list:
+        if idx < 0:
+            return (None)
+        elif idx > len(my_list):
+            return (None)
+        else:
+            print("Element at index {:d} is {}".format(idx, my_list[idx]))
